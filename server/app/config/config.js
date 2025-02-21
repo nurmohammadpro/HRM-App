74c4d1e4f212c1f52d91e15daee97093ec811d6a
Nur Mohammad <nur_mohammad1@outlook.com>
2025-02-21 15:40:42 +0600

    setting up the backend

---
+++ server/app/config/config.js
@@ -0,0 +1,17 @@
+export const MONGODB_CONNECTION = ""
+
+export const JWT_SECRET = ""
+export const JWT_EXPIRES_IN = ""
+
+export const EMAIL_HOST = ""
+export const EMAIL_PORT = ""
+export const EMAIL_USER = ""
+export const EMAIL_PASSWORD = ""
+
+export const MAX_JSON_SIZE = "10mb"
+export const URL_ENCODED = true
+
+export const REQUEST_LIMIT_TIME = "15m"
+export const REQUEST_LIMIT_COUNT = "100"    
+
+export const WEB_CACHE = false;
